17c5a0c415d6a21c030ea1fb27fe483c8a001c44
Omer Bey <omer.s.dai96@gmail.com>
2022-01-20 15:09:12 +0300

    guessing logic is implemented

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ const errorEl = document.getElementById('error');
 
 const guessCount = 6;
 const wordleLength = 5;
+const alphabetSet = new Set(alphabet);
+const worldeSet = new Set(wordleWords);
 
 let wordleWord;
 let guess; // number of guesses so far
 let wordIdx;
 let squares;
+let gameOver;
 
 function initializeGame() {
   for (let i = 0; i < guessCount; i++) {
@@ -26,10 +29,12 @@ function initializeGame() {
 }
 
 function startGame() {
-  wordleWord = wordleWords[Math.floor(Math.random() * wordleWords.length)];
+  //   wordleWord = wordleWords[Math.floor(Math.random() * wordleWords.length)];
+  wordleWord = 'ROBOT';
   guess = 0;
   wordIdx = 0;
   squares = [];
+  gameOver = false;
 
   guessEl
     .querySelectorAll('.row')
@@ -37,7 +42,7 @@ function startGame() {
 }
 
 function enterCharacter(char) {
-  if (!isTurkishCharacter(char) || wordIdx >= wordleLength) return;
+  if (gameOver || wordIdx >= wordleLength || !isTurkishCharacter(char)) return;
   char = char.toUpperCase();
 
   const square = squares[guess][wordIdx++];
@@ -46,9 +51,70 @@ function enterCharacter(char) {
 }
 
 function removeCharacter() {
+  if (gameOver || wordIdx === 0) return;
   const square = squares[guess][--wordIdx];
   square.innerText = '';
   square.classList.remove('active');
+}
+
+function enterWord() {
+  if (wordIdx < wordleLength) {
+    showError('Not enough words!');
+    return;
+  }
+  word = '';
+  squares[guess].forEach((square) => (word += square.innerText));
+  if (!worldeSet.has(word)) {
+    showError(word + ' is not a word!');
+  }
+
+  checkWord(word);
+  guess++;
+  wordIdx = 0;
+  if (word === wordleWord) {
+    // correctly guessed
+    showError('won!!!');
+    gameOver = true;
+  } else if (guess == guessCount) {
+    // out of guesses, lost
+    showError('lost!!!');
+    gameOver = true;
+  }
+}
+
+function checkWord(word) {
+  const freqTable = {};
+  const yellowSquares = [];
+  for (let i = 0; i < wordleLength; i++) {
+    const square = squares[guess][i];
+    const char = word[i];
+    const corr = wordleWord[i];
+
+    freqTable[corr] = freqTable[corr] === undefined ? 1 : freqTable[corr] + 1;
+    if (char === corr) {
+      square.className = 'square green';
+      freqTable[char] = freqTable[char] - 1;
+    } else if (wordleWord.includes(char)) {
+      yellowSquares.push(square); // potentially yellow or grey
+    } else {
+      square.className = 'square grey';
+    }
+  }
+  yellowSquares.forEach((square) => {
+    const char = square.innerText;
+    if (freqTable[char] > 0) {
+      freqTable[char] = freqTable[char] - 1;
+      square.className = 'square yellow';
+    } else {
+      square.className = 'square grey';
+    }
+  });
+}
+
+function showError(text) {
+  errorEl.innerText = text;
+  errorEl.classList.remove('hidden');
+  setTimeout(() => errorEl.classList.add('hidden'), 2000);
 }
 
 function keyPressed(e) {
@@ -62,23 +128,10 @@ function keyPressed(e) {
   }
 }
 
-function enterWord() {
-  if (wordIdx < wordleLength) {
-    showError('Not enough words!');
-    return;
-  }
-}
-
-function showError(text) {
-  errorEl.innerText = text;
-  errorEl.classList.remove('hidden');
-  setTimeout(() => errorEl.classList.add('hidden'), 2000);
-}
-
 keyElements.forEach((key) => key.addEventListener('click', keyPressed));
 
 function isTurkishCharacter(char) {
-  return char.length === 1 && alphabet.includes(char.toLowerCase());
+  return char.length === 1 && alphabetSet.has(char.toLowerCase());
 }
 
 initializeGame();
